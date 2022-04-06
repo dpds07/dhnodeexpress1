d7310b5bdf08f9d8f170c010260343eeba07f43d
Davi Silva <davi.silva@grupoeasytech.com>
2022-04-06 11:29:13 -0300

    feat: enviando HTML na resposta do servidor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = required('path')
 
 const app = express()
-const port = 3000
+const port = 3333
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
 
