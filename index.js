782db73d0b55dd468955ee23496535d093bbc78b
Davi Silva <davi.silva@grupoeasytech.com>
2022-04-06 11:32:17 -0300

    feat: enviando HTML na resposta do servidor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const path = required('path')
+const path = require('path')
 
 const app = express()
-const port = 3333
+const port = 3000
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views/index.html')))
 
 app.listen(port, () => console.info(`Servidor rodando na porta ${port}`))
